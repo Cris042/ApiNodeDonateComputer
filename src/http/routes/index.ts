fc04c17d2efe839a2ee28807616040cff9a76a05
Cristóvão Augusto <46114578+Cris042@users.noreply.github.com>
2022-07-29 15:55:50 -0300

    Update index.ts

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -4,6 +4,10 @@ import { donationRoutes } from "./donation.routes";
 
 const routes = Router();
 
+routes.get('/', function(req, res) {
+  res.status(201).json({ alive: true }).send();
+});
+
 routes.use("/donation", donationRoutes );
 
 export { routes };
